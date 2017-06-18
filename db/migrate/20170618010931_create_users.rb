01dba67d122162e081b86d6b781577e22830fe89
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2017-06-18 03:06:22 +0000

    first commit

---
+++ db/migrate/20170618010931_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string   "email",   limit: 50, null:false
+      t.string   "username",   limit: 50, null:false
+      t.string   "pass",       limit: 50, null:false
+      t.string   "role",       limit: 50, default: "admin", null:false
+      t.timestamps null: false
+    end
+  end
+end
