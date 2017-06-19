445b0ac5727d87e08341188f7105f5f7f575bf8c
chikashi <chikashi.ni.ym@gmail.com>
2017-06-20 08:14:45 +0900

    try rails v4 strong parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.fetch(:user, {})
+      # params.fetch(:user, {})
+      params.require(:user).permit(:id)
     end
 end
