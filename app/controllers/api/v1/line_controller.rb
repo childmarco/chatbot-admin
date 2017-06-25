2f92d71207864c0a2cd5dbd8479a7c4edb477b88
chikashi <chikashi.ni.ym@gmail.com>
2017-06-25 11:50:33 +0900

    modify contoller so that api can create user

--- app/controllers/api/v1/line_controller.rb
+++ app/controllers/api/v1/line_controller.rb
@@ -12,10 +12,43 @@ module Api
       end
 
       def show
-        @user = User.find(params[:id])
-        render json: @user
+        # @user = User.find(params[:id])
+        # render json: @user
+
+        # render json: UserResource.new(User.find(params[:id]))
+        render json: User.find(params[:id])
+
       end
 
+      def create
+        # @user = User.new(
+        #   email: params[:email],
+        #   role:  params[:username],
+        #   pass:  params[:pass],
+        #   role:  params[:role]
+        # )
+        # render json: @user
+
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+
+      end
+
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def user_params
+        params.require(:user).permit(:email, :username, :pass, :role)
+      end
     end
   end
 end
