c698ed8c284f1c43a34b4632bb1ffc92215dabb2
chikashi <chikashi.ni.ym@gmail.com>
2017-06-26 01:21:54 +0900

    bugix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :line
-      # post '/callback' => 'line#callback'
+      post '/callback' => 'line#callback'
     end
     namespace :v2 do
       resources :line
