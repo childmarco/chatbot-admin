50fcc5181aa05869234bcb5ca794bace28de7eed
chikashi.niiyama <chikashi.niiyama@accenture.com>
2017-10-28 04:53:11 +0900

    add legacy code via line sample

--- app/controllers/api/v1/line_controller.rb
+++ app/controllers/api/v1/line_controller.rb
@@ -11,15 +11,13 @@ module Api
       
       def client
         @client = Line::Bot::Client.new { |config|
-          config.channel_token    = ENV['LINE_CHANNEL_ID']
-          config.channel_secret   = ENV['LINE_CHANNEL_SECRET']
+          config.channel_token  = ENV['LINE_CHANNEL_ID']
+          config.channel_secret = ENV['LINE_CHANNEL_SECRET']
         }
       end
       
       def callback
         body = request.body.read
-        
-        logger.info(body)
         
         signature = request.env['HTTP_X_LINE_SIGNATURE']
         unless client.validate_signature(body, signature)
@@ -35,20 +33,23 @@ module Api
         
         
         events = client.parse_events_from(body)
+        
+        
+        logger.info("HELLO")
+        logger.info(events)
+        
+        
+        events = client.parse_events_from(body)
         events.each { |event|
           case event
             when Line::Bot::Event::Message
               case event.type
                 when Line::Bot::Event::MessageType::Text
-  
-                  logger.info("Hello startXXXX")
-                  reply_message = ApiUtilities::check_content(event.message['text'])
-                  logger.info("Hello startYYYY")
-                  # message = {
-                  #   type: 'text',
-                  #   text: event.message['text']
-                  # }
-                  client.reply_message(event['replyToken'], reply_message)
+                  message = {
+                    type: 'text',
+                    text: event.message['text']
+                  }
+                  client.reply_message(event['replyToken'], message)
                 when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
                   response = client.get_message_content(event.message['id'])
                   tf       = Tempfile.open("content")
@@ -56,6 +57,30 @@ module Api
               end
           end
         }
+        
+        "OK"
+          
+          # events.each { |event|
+          #   case event
+          #     when Line::Bot::Event::Message
+          #       case event.type
+          #         when Line::Bot::Event::MessageType::Text
+          #
+          #           logger.info("Hello startXXXX")
+          #           reply_message = ApiUtilities::check_content(event.message['text'])
+          #           logger.info("Hello startYYYY")
+          #           message = {
+          #             type: 'text',
+          #             text: event.message['text']
+          #           }
+          # client.reply_message(event['replyToken'], reply_message)
+          # when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          #   response = client.get_message_content(event.message['id'])
+          #   tf       = Tempfile.open("content")
+          #   tf.write(response.body)
+          # end
+          # end
+          # }
       end
       
       private
