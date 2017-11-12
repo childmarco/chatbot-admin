fda631ec4b45ec9b6d617599e577c4e9caba41fa
chikashi.niiyama <chikashi.ni.ym@gmail.com>
2017-11-13 04:47:55 +0900

    debug

--- app/controllers/api/v1/line_controller.rb
+++ app/controllers/api/v1/line_controller.rb
@@ -31,7 +31,6 @@ module Api
         signature = request.env['HTTP_X_LINE_SIGNATURE']
         
         unless client.validate_signature(body, signature)
-          logger.info("Hello signature 2")
           error 400 do
             'Bad Request'
           end
@@ -49,21 +48,31 @@ module Api
           line_user_id = event['source'].fetch('type', nil) == "user" ? event['source']['userId'] : nil
           
           if line_user_id.present?
+            
             case event
               when Line::Bot::Event::Message
                 case event.type
                   #テキストメッセージが送られた場合、そのままおうむ返しする
                   when Line::Bot::Event::MessageType::Text
-                    # logger.info(event)
-                    # reply_message = ApiUtilities::check_content(event.message['text'])
-                    reply_message = ApiUtilities::list_button
-                  # reply_message = ApiUtilities::confirm_button
-                  # reply_message = ApiUtilities::text_carousel
-                  # reply_message = ApiUtilities::imaga_carousel
-                  # message = {
-                  #   type: 'text',
-                  #   text: event.message['text']
-                  # }
+                    
+                    if User.find_by(lineId: line_user_id).nil?
+                      #   LineアカウントとUser登録情報の紐付け
+                      reply_message = ApiUtilities::check_content("ご利用いただきありがとうございます。\n電話番号を入力してくだいさい。")
+                    
+                    else
+                      
+                      reply_message = ApiUtilities::list_button
+                      
+                      # reply_message = ApiUtilities::check_content(event.message['text'])
+                      # reply_message = ApiUtilities::confirm_button
+                      # reply_message = ApiUtilities::text_carousel
+                      # reply_message = ApiUtilities::imaga_carousel
+                    
+                    end
+                  
+                  else
+                    reply_message = ApiUtilities::check_content("現在未対応です。")
+                
                 end
                 client.reply_message(event['replyToken'], reply_message)
             end
@@ -71,8 +80,6 @@ module Api
           
           end
         }
-        
-        logger.info(events)
         
         head :ok
       end
